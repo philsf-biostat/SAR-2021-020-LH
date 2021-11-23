88f335ad77a678d67e3d671dc129bfb73e72e56b
Felipe Figueiredo <philsf79@gmail.com>
2021-11-23 13:46:55 -0300

    analytical data mockup (excel created, with dictionary)

--- scripts/input.R
+++ scripts/input.R
@@ -7,10 +7,9 @@ library(labelled)
 
 # data loading ------------------------------------------------------------
 set.seed(42)
-data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
-# data.raw <- read_excel("dataset/file.xlsx") %>%
-#   janitor::clean_names()
-
+# data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
+data.raw <- read_excel("dataset/il6-circadian.xlsx") %>%
+  janitor::clean_names()
 
 # data cleaning -----------------------------------------------------------
 
@@ -31,7 +30,7 @@ data.raw <- data.raw %>%
 data.raw <- data.raw %>%
   set_variable_labels(
     group = "Study group",
-    outcome = "Study outcome",
+    # outcome = "Study outcome",
   )
 
 # analytical dataset ------------------------------------------------------
@@ -41,11 +40,11 @@ analytical <- data.raw %>%
   select(
     id,
     group,
-    outcome,
+    # outcome,
   )
 
 # mockup of analytical dataset for SAP and public SAR
 analytical_mockup <- tibble( id = c( "1", "2", "3", "...", as.character(nrow(analytical)) ) ) %>%
-  left_join(analytical %>% head(0), by = "id") %>%
+  left_join(data.raw %>% head(0), by = "id") %>%
   mutate_all(as.character) %>%
   replace(is.na(.), "")
