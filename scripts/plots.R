c28859d2336719a7cdb1907f92ee8d05c3a2f806
Felipe Figueiredo <philsf79@gmail.com>
2021-11-23 12:13:15 -0300

    Initial commit

---
+++ scripts/plots.R
@@ -0,0 +1,33 @@
+# setup -------------------------------------------------------------------
+# library(ggplot2)
+# library(survminer)
+
+ff.col <- "steelblue" # good for single groups scale fill/color brewer
+ff.pal <- "Paired"    # good for binary groups scale fill/color brewer
+
+# Theme setting (less is more)
+theme_set(
+  theme_classic()
+)
+theme_update(
+  legend.position = "top"
+)
+
+gg <- ggplot(analytical) +
+  scale_color_brewer(palette = ff.pal) +
+  scale_fill_brewer(palette = ff.pal)
+
+# plots -------------------------------------------------------------------
+
+gg.outcome <- gg +
+  geom_density(aes(outcome, fill = group), alpha = .8) +
+  xlab(attr(analytical$outcome, "label")) +
+  ylab("")
+
+# cool facet trick from https://stackoverflow.com/questions/3695497 by JWilliman
+# gg +
+#   geom_histogram(bins = 5, aes(outcome, y = ..count../tapply(..count.., ..PANEL.., sum)[..PANEL..]), fill = ff.col) +
+#   scale_y_continuous(labels = scales::label_percent(accuracy = 1)) +
+#   xlab(attr(analytical$outcome, "label")) +
+#   ylab("") +
+#   facet_wrap(~ group, ncol = 2)
